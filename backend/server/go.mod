6e2e36233790d0307187f14995ce9e6e39441d99
Khinshan Khan <khanshan501@gmail.com>
2021-07-25 00:51:32 -0400

    meta information is now the home endpoint of application

--- backend/server/go.mod
+++ backend/server/go.mod
@@ -2,6 +2,9 @@ module github.com/kkhan01/caputo/backend/server
 
 go 1.16
 
-require github.com/kkhan01/caputo/backend/goutils v0.0.0-00010101000000-000000000000
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/kkhan01/caputo/backend/goutils v0.0.0-00010101000000-000000000000
+)
 
 replace github.com/kkhan01/caputo/backend/goutils => ../goutils
--- backend/server/main.go
+++ backend/server/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
-	"github.com/kkhan01/caputo/backend/goutils"
-	metausecases "github.com/kkhan01/caputo/backend/server/usecases/meta"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 // Version and BuildData get replaced during build with the commit hash and time
@@ -12,6 +18,44 @@ var (
 	BuildDate  = ""
 )
 
+// Meta holds project metadata
+type Meta struct {
+	CommitHash string    `json:"commitHash"`
+	BuildDate  string    `json:"buildDate"`
+	SystemTime time.Time `json:"systemTime"`
+}
+
 func main() {
-	goutils.PrettyPrint(metausecases.GetMeta(CommitHash, BuildDate))
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", meta)
+
+	port := 5000
+	if p := os.Getenv("PORT"); p != "" {
+		_, err := fmt.Sscanf(p, "%d", &port)
+		if err != nil {
+			log.Fatal("Invalid PORT in env")
+		}
+	}
+
+	fmt.Printf("Listening on port %d\n", port)
+	log.Fatal(
+		http.ListenAndServe(fmt.Sprintf(":%d", port), router),
+	)
+}
+
+func meta(w http.ResponseWriter, r *http.Request) {
+	currentTimestamp := time.Now().UTC()
+
+	metainfo := Meta{
+		BuildDate:  BuildDate,
+		CommitHash: CommitHash,
+		SystemTime: currentTimestamp,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(metainfo); err != nil {
+		log.Println(err.Error())
+	}
 }
