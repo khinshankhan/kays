d4dde9da0a26b655009fad1e42bd14664c67539a
Shakil Rafi <19642235+srafi1@users.noreply.github.com>
2021-08-12 16:32:49 -0400

    view images from frontend

--- frontend/website/pages/index.tsx
+++ frontend/website/pages/index.tsx
@@ -1,8 +1,21 @@
 import { Heading } from "@chakra-ui/react";
 import { NextPage } from "next";
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import FileGrid from "../components/Grid";
+import { FileMeta } from "../util/file";
 
 const Index: NextPage = () => {
+  const [files, setFiles] = useState<FileMeta[]>([]);
+
+  useEffect(() => {
+    fetch('/files')
+      .then(res => res.json())
+      .then(json => {
+        setFiles(json);
+      })
+  });
+
   return (
     <>
       <Head>
@@ -10,8 +23,10 @@ const Index: NextPage = () => {
       </Head>
 
       <Heading as="h1" size="2xl" mb="2">
-        Hello World!
+        Caputo
       </Heading>
+
+      <FileGrid files={files} />
     </>
   );
 };
